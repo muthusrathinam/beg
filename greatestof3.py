8bb507138377c62dba7d5ff764a65a799c2f7413
muthusrathinam <51087937+muthusrathinam@users.noreply.github.com>
2019-07-22 21:57:34 +0530

    Add files via upload

---
+++ greatestof3.py
@@ -0,0 +1,9 @@
+a=int(input("enter value of a:"))
+b=int(input("enter value of b:"))
+c=int(input("enter value of c:"))
+if(a>b and a>c):
+     print("a is greater")
+elif(b>c):
+     print("b is greater")
+else:
+     print("c is greater")
---
+++ leap.py
@@ -0,0 +1,5 @@
+yr=int(input("enter year:"))
+if yr%4==0:
+     print("entered yr is leap year")
+else:
+     print("entered yr is not a leap year")
